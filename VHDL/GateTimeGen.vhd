a0b90a7fec993a3f0fe2d4239a8c513d9df17c59
LUMERIIX <lukas.jenni@comlab.ch>
2018-01-09 15:48:37 +0100

    small changes for ghdl simulation

--- VHDL/GateTimeGen.vhd
+++ VHDL/GateTimeGen.vhd
@@ -6,7 +6,7 @@
 -- Design Name: 
 -- Module Name: GateTimeGen   
 -- Project Name: FrequnecyCounter
--- Target Devices: iCE40 
+-- Target Devices: Spartan LX 9 
 -- Tool versions: 
 -- Description: 
 --
@@ -27,36 +27,41 @@ entity GateTimeGen is
 	-- );
 	port ( 
 		CLK 			: in STD_LOGIC;
+		RESET           : in STD_LOGIC;
 		GatePulse		: out STD_LOGIC;
 		GateReady		: in STD_LOGIC;
-		CountLimit		: in natural;
+		CountLimit		: in unsigned(30 downto 0);
 		Valid			: out STD_LOGIC
 		);
 end GateTimeGen;
 
 architecture Behavioral of GateTimeGen is
 
-	signal Count_s : unsigned (30 downto 0);
-	signal GatePulse_s : std_logic;
-	signal GateReady_s : std_logic;
-	signal Valid_s : std_logic;
-	signal CountLimit_s : natural;
- 
+	signal Count_s : unsigned (30 downto 0) := to_unsigned(0,31);
+	signal GatePulse_s : std_logic := '0';
+	signal GateReady_s : std_logic := '0';
+	signal Valid_s : std_logic := '0';
+	signal CountLimit_s : unsigned(30 downto 0) := to_unsigned(0,31);
+
 begin
 	Valid <= Valid_s;
 	GatePulse <= GatePulse_s;
 	CountLimit_s <= CountLimit;
 	GateReady_s <= GateReady;
-	
-	
-	TimeGen : process (CLK) begin
+
+
+	TimeGen : process (CLK,RESET) begin
+--	    if (RESET = '1') then
+--            GatePulse_s <= '0';
+--            GateReady_s <= '0';
+--            Valid_s <= '0';
 		if rising_edge(CLK) then
 			if GateReady_s = '1' and Valid_s = '0'  then
 				GatePulse_s <= '1';
-				if Count_s = to_unsigned(CountLimit_s, Count_s'length) then
+				if Count_s = CountLimit_s then
 					Count_s <= to_unsigned(0,Count_s'length);
 					GatePulse_s <= '0';
-					Valid_s <= '1'; 
+					Valid_s <= '1';
 				else
 					Count_s <= Count_s +1;
 				end if;
