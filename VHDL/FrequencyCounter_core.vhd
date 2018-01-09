ea2a00905eeb9646213cbe4ebe2e498f309bdc44
LUMERIIX <lukas.jenni@comlab.ch>
2018-01-09 15:52:40 +0100

    small code refactoring & bug fixes & clean up

--- VHDL/FrequencyCounter_core.vhd
+++ VHDL/FrequencyCounter_core.vhd
@@ -26,6 +26,7 @@ entity FrequencyCounter_core is
 		CLK 			: in STD_LOGIC;
 		MEAS_CLK		: in STD_LOGIC;
 		GateEnable	 	: in STD_LOGIC;
+		CounterReset    : in STD_LOGIC;
 		RefCountVal		: out std_logic_vector (31 downto 0);
 		GatePulse		: in  STD_LOGIC;
 		TEST 			: out STD_LOGIC;
@@ -49,18 +50,30 @@ architecture structure of FrequencyCounter_core is
 		DATA		: out std_logic_vector(31 downto 0)
 		);
 	end component Counter;
-	signal OpenGate_s : STD_LOGIC;
-	signal RefCountVal_s : unsigned (31 downto 0);
-	signal MeasCountVal_s : unsigned (31 downto 0);
-	signal DataValidRef_s : STD_LOGIC;
-	signal DataValidMeas_s : STD_LOGIC;
-	signal DataValid_s : STD_LOGIC;
+	signal OpenGate_s : STD_LOGIC := '0';
+	signal RefCountVal_s : unsigned (31 downto 0) := to_unsigned(0,32);
+	signal MeasCountVal_s : unsigned (31 downto 0) := to_unsigned(0,32);
+	signal DataValidRef_s : STD_LOGIC := '0';
+	signal DataValidMeas_s : STD_LOGIC := '0';
+	signal DataValid_s : STD_LOGIC := '0';
 begin
 	DataValid_s <= DataValidMeas_s and DataValidRef_s;
 	--RefCountVal <= RefCountVal_s;
 	--MeasCountVal <= MeasCountVal_s;
 	OpenGate <= OpenGate_s;
 	DataValid <= DataValid_s;
+
+    Init : process (RESET)
+	begin
+        if rising_edge (RESET) then
+            --OpenGate_s <= '0';
+            --ountVal_s <= to_unsigned(0,RefCountVal_s'length);
+            --MeasCountVal_s <= to_unsigned(0,MeasCountVal_s'length);
+            --DataValidRef_s <= '0';
+            --DataValidMeas_s <= '0';
+            --DataValid_s <= '0';
+        end if;
+	end process;
 
 	GateControl : process (MEAS_CLK) begin
 		if rising_edge (MEAS_CLK)  then
@@ -79,7 +92,7 @@ begin
 		ENABLE => OpenGate_s,
 		TEST => open,
 		DataValid => DataValidRef_s,
-		RESET => RESET,
+		RESET => CounterReset,
 		DATA => RefCountVal
 	);
 
@@ -89,7 +102,7 @@ begin
 		ENABLE => OpenGate_s,
 		TEST => open,
 		DataValid => DataValidMeas_s,
-		RESET => RESET,
+		RESET => CounterReset,
 		DATA => MeasCountVal
 	);
 end architecture structure;
